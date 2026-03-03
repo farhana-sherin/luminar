770a03c0a077f5a5fae1390beb42244139cd30dc
farhan sherin <farhana23sherin9023@gmail.com>
2026-03-03 15:09:01 +0530

    initial commit

---
+++ frontend/src/Layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
+
+
+export const MainLayout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
