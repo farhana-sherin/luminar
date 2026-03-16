ad5e8a5048447a4c298a3f2877b1cf9b1226df32
farhan sherin <farhana23sherin9023@gmail.com>
2026-03-16 13:00:06 +0530

    build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+# Install backend dependencies
 pip install -r requirements.txt
+
+# Collect static files (includes the built React app)
 python manage.py collectstatic --noinput
+
+# Run migrations
 python manage.py migrate
 
